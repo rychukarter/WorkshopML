65da5066ef97e1369e0af33b2105b6c89689b956
rychukarter <rychter.karol@gmail.com>
2018-11-14 13:48:04 +0100

    added Gradient descent and cost fuction implementation

--- venv/include/main.py
+++ venv/include/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from utilities import utilities as utl
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.linear_model import LinearRegression
@@ -8,13 +9,13 @@ from sklearn.model_selection import train_test_split
 
 # Read data from file (into DataFrame)
 data = pd.read_excel("mlr03.xls")
-data = shuffle(data)
+# data = shuffle(data)
 # Print data
 print(data)
 
 # Plot data and show the plot
 data.plot()
-plt.show()
+
 
 # Advance plotting
 plt.figure()
@@ -25,10 +26,10 @@ plt.plot(X["EXAM2"], Y, "go")
 plt.plot(X["EXAM3"], Y, "bo")
 plt.show()
 
+
 # Data scaling
 X_scaled = utl.scale_df(X)
 print(X_scaled)
-
 column_names = X.columns.values
 scaler = MinMaxScaler()
 X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=column_names, index=data.index)
@@ -37,13 +38,32 @@ print(X_scaled)
 # Data split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, test_size=0.2, shuffle=False)
 
+
+theta = np.array([1.0, 1.0, 1.0])
+num_iters = 1000
+alpha = 0.00001
+
+J = utl.cost_function(X_train, Y_train, theta)
+print("Starting theta: ", theta)
+print("Starting J = ", J)
+theta = utl.gradient_descent(X_train, Y_train, theta, alpha, num_iters)
+J = utl.cost_function(X_train, Y_train, theta)
+print("After GD theta: ", theta)
+print("After GD J = ", J)
+Y_predicted = np.dot(X_test, theta)
+print(Y_predicted)
+
+
 # Linear Regression training
 regression = LinearRegression(normalize=False)
 regression.fit(X_train, Y_train)
-
+J = utl.cost_function(X_train, Y_train, regression.coef_)
+print("Theta calculated by sklear: ", regression.coef_)
+print("J of sklearn: ", J)
 # Linear Regression test
-Y_predicted = regression.predict(X_test)
+Y_predicted_sk = regression.predict(X_test)
 plt.figure()
-plt.plot(X_test["EXAM1"], Y_test, "ro")
-plt.plot(X_test["EXAM1"], Y_predicted, "bx")
+plt.plot(X_test["EXAM1"], Y_test, "go")
+plt.plot(X_test["EXAM1"], Y_predicted_sk, "bx")
+plt.plot(X_test["EXAM1"], Y_predicted, "rx")
 plt.show()
--- venv/include/utilities/utilities.py
+++ venv/include/utilities/utilities.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def scale_df(data):
@@ -6,5 +7,20 @@ def scale_df(data):
     return data.apply(lambda x: (x - data.min())/(data.max() - data.min()), axis=1)
 
 
+def cost_function(X, y, theta):
+    return (1/(2*y.size)) * np.sum((np.dot(X.values, theta).T - y.values) ** 2)
+
+
+def gradient_descent(X, y, theta, alpha, num_iters):
+    theta_temp = theta
+    for i in range(num_iters):
+        for n in range(theta.size):
+            delta = np.sum((np.dot(X.values, theta).T - y.values) * X.values[:, n])
+            theta_temp[n] = theta[n] - (alpha/y.size) * delta
+        theta = theta_temp
+    return theta
+
+
 def linear_regression():
+
     pass
