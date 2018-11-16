718dd24d53db34db9957847710e61dfaa4330869
Pawel Borawski <pawel.borawski2@orange.com>
2018-11-16 10:13:17 +0100

    SVM codes ready for the show

--- venv/include/poker_classification.py
+++ venv/include/poker_classification.py
@@ -1,69 +1,74 @@
-# 1. Import data as data frame
+# 0. Imports
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import time
+from sklearn.svm import SVC
+
+# 1. Import data as data frame
 print("Step 1: import data as data frame")
 
 df = pd.read_csv("data/poker/poker-hand-data.csv")
-df = df.sample(n=50000, random_state=100)
+df = df.sample(n=10000, random_state=100)
 print(df.info())
 print(df.head())
 
 
-# 2. Data normalization and scaling
-from sklearn.preprocessing import MinMaxScaler
-print("\nStep 2: Data normalization and scaling")
-
-scaler = MinMaxScaler()
-scaled_df = pd.DataFrame(scaler.fit_transform(df), columns=list(df.columns.values))
-print(scaled_df.head())
-
-
-# 3. Train test split
-from sklearn.model_selection import train_test_split
-print("\nStep 3: split data by train test split")
-
-X = np.array(scaled_df.drop('Class', axis=1))
-y = np.array(df['Class'])
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
-print(X_train.size)
-print(X_test.size)
-
-# 4. Linear SVM method
-from sklearn.svm import LinearSVC
-import time
-print("\nStep 4: linear SVM classifier training")
-
-start = time.time()
-lin_cls = LinearSVC(C=0.5, random_state=100)
-lin_cls.fit(X_train, y_train)
-end = time.time()
-print("Learning time: " + str(int(end - start)) + " seconds")
-
-# 4a. Linear SVM evaluation
-print("\nStep 4a: linear SVM classifier evaluation")
-y_predict = lin_cls.predict(X_test)
-errors = 0
-for i in range(0, y_test.size):
-    if y_predict[i] != y_test[i]:
-        errors += 1
-print("Accuracy: " + str(1 - errors/np.size(y_test)))
-
-# 5. Kernel based SVM
-from sklearn.svm import SVC
-print("\nStep 5: nonlinear SVM classifier training")
-
-start = time.time()
-rbf_cls = SVC(C=0.5, kernel='rbf', random_state=100)
-rbf_cls.fit(X_train, y_train)
-end = time.time()
-print("Learning time: " + str(int(end - start)) + " seconds")
+# # 2. Data normalization and scaling
+# print("\nStep 2: Data normalization and scaling")
+#
+# scaler = MinMaxScaler()
+# scaled_df = pd.DataFrame(scaler.fit_transform(df), columns=list(df.columns.values))
+# print(scaled_df.head())
 
 
-# 5a. Kernel based SVM evaluation
-print("\nStep 4a: nonlinear SVM classifier evaluation")
-y_predict = rbf_cls.predict(X_test)
-errors = 0
-for i in range(0, y_test.size):
-    if y_predict[i] != y_test[i]:
-        errors += 1
-print("Accuracy: " + str(1 - errors/np.size(y_test)))
+# # 3. Train test split
+# print("\nStep 3: split data by train test split")
+#
+# X = np.array(scaled_df.drop('Class', axis=1))
+# y = np.array(df['Class'])
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+# print(X_train.size)
+# print(X_test.size)
+
+# # 4. Linear SVM method
+# from sklearn.svm import LinearSVC
+# print("\nStep 4: linear SVM classifier training")
+#
+# start = time.time()
+# lin_cls = LinearSVC(C=5.0, random_state=100)
+# lin_cls.fit(X_train, y_train)
+# end = time.time()
+# print(lin_cls.get_params())
+# print("Learning time: " + str(int(end - start)) + " seconds")
+
+# # 4a. Linear SVM accuracy evaluation
+# print("\nStep 4a: linear SVM classifier accuracy evaluation")
+# y_predict = lin_cls.predict(X_test)
+# errors = 0
+# for i in range(0, y_test.size):
+#     if y_predict[i] != y_test[i]:
+#         errors += 1
+# print("Accuracy: " + str(1 - errors/np.size(y_test)))
+# print("Accuracy by library: " + str(lin_cls.score(X_test, y_test)))
+
+# # 5. Kernel based SVM
+# print("\nStep 5: nonlinear SVM classifier training")
+#
+# start = time.time()
+# rbf_cls = SVC(C=5.0, kernel='rbf', random_state=100)
+# rbf_cls.fit(X_train, y_train)
+# end = time.time()
+# print("Learning time: " + str(int(end - start)) + " seconds")
+
+
+# # 5a. Kernel based SVM evaluation
+# print("\nStep 5a: nonlinear SVM classifier evaluation")
+# y_predict = rbf_cls.predict(X_test)
+# errors = 0
+# for i in range(0, y_test.size):
+#     if y_predict[i] != y_test[i]:
+#         errors += 1
+# print("Accuracy: " + str(1 - errors/np.size(y_test)))
+# print("Accuracy by library: " + str(rbf_cls.score(X_test, y_test)))
--- venv/include/svm_intro.py
+++ venv/include/svm_intro.py
@@ -2,23 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import LinearSVC
 
+# 1. Prepare some fake data
 data = np.array([[1, 2], [2, 1], [2, 2], [3, 4], [4, 3], [3, 3]])
 labels = ["red", "red", "red", "blue", "blue", "blue"]
-
 plt.scatter(data[:, 0], data[:, 1], c=labels)
-# plt.show()
-
-svm_classifier = LinearSVC()
-svm_classifier.fit(data, labels)
-print(svm_classifier.predict([[3, 1]]))
-
-# Model explanation
-print(svm_classifier.coef_)
-print(svm_classifier.intercept_)
-
-w = svm_classifier.coef_[0]
-a = -w[0] / w[1]
-x = np.linspace(0, 5)
-y = a * x - svm_classifier.intercept_[0] / w[1]
-plt.plot(x, y)
 plt.show()
+
+# # 2. Create and fit linear SVM Classifier
+# svm_classifier = LinearSVC(C=100, verbose=1)
+# svm_classifier.fit(data, labels)
+# print(svm_classifier.get_params())
+
+# # 3. Make some test prediction
+# print(svm_classifier.predict([[3, 1]]))
+
+# # 4. Model explanation
+# print(svm_classifier.coef_)
+# print(svm_classifier.intercept_)
+
+# # 5. Plot decision plane
+# w = svm_classifier.coef_[0]
+# a = -w[0] / w[1]
+# x = np.linspace(0, 5)
+# y = a * x - svm_classifier.intercept_[0] / w[1]
+# plt.plot(x, y)
+# plt.show()
